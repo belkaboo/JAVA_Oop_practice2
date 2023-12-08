10d9229b95fcae34f48540becefd21a86219ba16
belka.boo <ibelka.boo@gmail.com>
2023-12-08 11:29:28 +0300

    class Market done(not checked)

--- src/Market.java
+++ src/Market.java
@@ -3,45 +3,73 @@ import java.util.List;
 
 public class Market implements MarketBehaviour, QueueBehaviour {
     private final List<Actor> queue;
+    private final List<Actor> releasedFromQueue;
 
     public Market() {
         this.queue = new ArrayList<>();
+        this.releasedFromQueue = new ArrayList<>();
     }
 
     @Override
     public void acceptToMarket(Actor actor) {
-        //todo
+        System.out.println(actor.getName() + " -  в магазине");
     }
 
     @Override
     public void takeInQueue(Actor actor) {
-        //todo
+        System.out.println(actor.getName() + " - в Очереди");
+        queue.add(actor);
     }
 
     @Override
     public void takeOrders() {
-        //todo
+        for (Actor actor: queue){
+            if (!actor.isMakeOrder()){
+                actor.setMakeOrder(true);
+                System.out.println(actor.getName() + " - заказ сделан");
+            }
+        }
 
     }
 
     @Override
     public void giveOrders() {
-        //todo
+        for (Actor actor: queue){
+            if (!actor.isTakeOrder()){
+                actor.setTakeOrder(true);
+                System.out.println(actor.getName() + " - заказ получен");
+            }
+        }
     }
-
 
     @Override
     public void releaseFromQueue() {
-        //todo
+        for (Actor actor: queue){
+            if (actor.isTakeOrder){
+                releasedFromQueue.add(actor);
+                System.out.println(actor.getName() + " - покинул очередь");
+            }
+
+        }
+        releaseFromMarket(releasedFromQueue);
     }
+
     @Override
     public void releaseFromMarket(List<Actor> actors) {
-        //todo
+        for (Actor actor: actors){
+
+            System.out.println(actor.getName() + " - покинул магазин");
+            queue.remove(actor);
+
+        }
     }
 
     @Override
     public void update() {
-        //todo
+        takeOrders();
+        giveOrders();
+        releaseFromQueue();
+
     }
 
 }
